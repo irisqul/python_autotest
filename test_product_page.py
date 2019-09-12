d472065adbca03aed2f9ac66886b43c08858980d
Irisqul <irina.rudnichenko@gmail.com>
2019-09-12 10:17:44 +0300

    tests are grouped into classes

--- test_product_page.py
+++ test_product_page.py
@@ -18,41 +18,43 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.product_name_and_added_product_name_are_the_same()
     page.product_price_and_added_product_price_are_the_same()
 
-@pytest.mark.xfail(reason="This is a demonstration of XFAIL mark usage")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    time.sleep(3)
-    page.is_not_element_present(*ProductPageLocators.PRODUCT_IS_ADDED_MESSAGE)
+@pytest.mark.success_message
+class TestSuccessMassageForGuest():
+    @pytest.mark.xfail(reason="This is a demonstration of XFAIL mark usage")
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        time.sleep(3)
+        page.is_not_element_present(*ProductPageLocators.PRODUCT_IS_ADDED_MESSAGE)
 
-@pytest.mark.xfail(reason="Another demonstration of XFAIL mark usage")
-def test_success_message_dissapear_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.solve_quiz_and_get_code()
-    time.sleep(3)
-    page.is_disappeared(*ProductPageLocators.PRODUCT_IS_ADED_MESSAGE)
+    @pytest.mark.xfail(reason="Another demonstration of XFAIL mark usage")
+    def test_success_message_dissapear_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        time.sleep(3)
+        page.is_disappeared(*ProductPageLocators.PRODUCT_IS_ADED_MESSAGE)
 
+@pytest.mark.login_guest
+class TestLoginFromProductPage():
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-@pytest.mark.smoke
-def test_guest_can_go_to_login_page_from_product_page (browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_url()
 
 @pytest.mark.smoke
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
